e09e76d65800e586bc7e2680ca86ca82d6ab2519
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-11-19 09:31:42 +0530

    adding pages

--- pages/books_price.jsx
+++ pages/books_price.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-
-
+import React, { useState, useEffect} from "react";
+import Product from "../components/Product";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 const books_price = () => {
+  const [products, setProducts] = useState([]);
+  const { publicKey, sendTransaction } = useWallet();
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
   return (
-    <div>
-        books_price
+    <div >
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   )
 }
